0b8078b819e20dc60fa073f82cad81a9279c3b54
yijiagu123 <yijia.gu@gwmail.gwu.edu>
2024-02-12 21:51:03 -0500

    Create IndexController.java

---
+++ springboot-capstone-team100/src/main/java/com/team100/springboot/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.team100.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    // HTTP GET Request
+    // http://localhost:8080
+    @GetMapping("/")
+    public String indexView(){
+        return "index";
+    }
+}
