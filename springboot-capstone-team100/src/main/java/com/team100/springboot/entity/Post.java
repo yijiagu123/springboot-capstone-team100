dea50d305400424485e967a774e92647312a2053
yijiagu123 <yijia.gu@gwmail.gwu.edu>
2024-04-22 13:22:44 -0400

    Create Post.java

---
+++ springboot-capstone-team100/src/main/java/com/team100/springboot/entity/Post.java
@@ -0,0 +1,36 @@
+package com.team100.springboot.entity;
+
+import lombok.*;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(
+        name = "post",
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})}
+)
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long postId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user; // Assuming there is a User entity mapped to 'users' table
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "post_at", nullable = false)
+    private LocalDateTime postAt; // Uses java.time.LocalDateTime for timestamp
+
+}
+
