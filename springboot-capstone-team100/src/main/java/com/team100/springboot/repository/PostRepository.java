a2f5b587184e14f56176314e73c561c8c4e3a8b6
yijiagu123 <yijia.gu@gwmail.gwu.edu>
2024-04-22 20:18:24 -0400

    Create PostRepository.java

---
+++ springboot-capstone-team100/src/main/java/com/team100/springboot/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.team100.springboot.repository;
+
+import com.team100.springboot.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
