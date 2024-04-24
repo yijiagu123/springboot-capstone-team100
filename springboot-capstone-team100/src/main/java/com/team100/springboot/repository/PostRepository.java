f6e7f3d5270995a08af765fa110d5cbf791ab6c2
yijiagu123 <yijia.gu@gwmail.gwu.edu>
2024-04-23 22:21:53 -0400

    Update PostRepository.java

--- springboot-capstone-team100/src/main/java/com/team100/springboot/repository/PostRepository.java
+++ springboot-capstone-team100/src/main/java/com/team100/springboot/repository/PostRepository.java
@@ -1,10 +1,17 @@
 package com.team100.springboot.repository;
 
 import com.team100.springboot.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Page<Post> findAllByTitleContainingIgnoreCaseOrContentContainingIgnoreCase(String titleKeyword, String contentKeyword, Pageable pageable);
 
 }
