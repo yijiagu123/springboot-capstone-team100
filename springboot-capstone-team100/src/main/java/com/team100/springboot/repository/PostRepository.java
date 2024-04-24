76a3d0e06d15451f282a6c764663e84b2fa10df0
yijiagu123 <yijia.gu@gwmail.gwu.edu>
2024-04-23 22:25:26 -0400

    Update PostRepository.java

--- springboot-capstone-team100/src/main/java/com/team100/springboot/repository/PostRepository.java
+++ springboot-capstone-team100/src/main/java/com/team100/springboot/repository/PostRepository.java
@@ -4,9 +4,6 @@ import com.team100.springboot.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
