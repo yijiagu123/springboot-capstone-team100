d6210bd0f913e0b5468c64e04938798bcd3419f2
Borreload Savage Dragon <104666660+RokketTracer@users.noreply.github.com>
2024-04-29 20:55:28 -0400

    Create SubscriptionController.java

---
+++ springboot-capstone-team100/src/main/java/com/team100/springboot/controller/SubscriptionController.java
@@ -0,0 +1,11 @@
+package com.team100.springboot.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class SubscriptionController {
+
+    @GetMapping("/goToSubscription")
+    public String goToSubscription() {
+        return "redirect:/subscription.html";
+    }
+}
